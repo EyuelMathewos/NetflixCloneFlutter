017c5fed4d8bb2c0484bab1f9f1211f900a20914
FlutLab.io <>
2021-06-24 13:22:04 +0000

    Autosave

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -117,10 +117,6 @@ class searchState extends State<Search> {
                   ), //Row
                   Container(
                     height: 20,
-                    child: TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Enter your email',
-            ),
                   ),
                   SingleChildScrollView(
                     scrollDirection: Axis.vertical,

5ff964c7fbcd26a7178fca41e1bae93e61c80bdc
FlutLab.io <>
2021-06-25 18:40:40 +0000

    Autosave

--- lib/JSON/movie.dart
+++ lib/JSON/movie.dart
@@ -1,37 +1,9 @@
-import 'assets/images/oldguardcover.jpg' as oldguardcover;
-import 'assets/images/OldGuardfeature.jpg' as oldguardfeature;
-import 'assets/images/oldguardtitle.png' as oldguardtitle;
-
-import 'assets/images/assassinscreedcover.jpg' as assassincover;
-import 'assets/images/assassinscreedfeature.jpg' as assassinfeature;
-import 'assets/images/assassinscreedtitle.png' as assassintitle;
-
-import 'assets/images/spider-mancover.jpg' as spidercover;
-import 'assets/images/spider-manfeature.jpg' as spiderfeature;
-import 'assets/images/spidermantitle.png' as spidertitle;
-
-import 'assets/images/venomcover.jpg' as venomcover;
-import 'assets/images/venomfeature.jpg' as venomfeature;
-import 'assets/images/venomtitle.png' as venomtitle;
-
-import 'assets/images/projectpowercover.jpg' as projectpowercover;
-import 'assets/images/projectpowerfeature.jpg' as projectpowerfeature;
-import 'assets/images/powerprojecttitle.png' as projectpowertitle;
-
-import 'assets/images/bloodshotcover.jpg' as bloodshotcover;
-import 'assets/images/bloodshotfeature.jpg' as bloodshotfeature;
-import 'assets/images/bloodshottitle.png' as bloodshottitle;
-
-import 'assets/images/lucycover.jpg' as lucycover;
-import 'assets/images/lucyfeature.jpg' as lucyfeature;
-import 'assets/images/lucytitle.png' as lucytitle;
-
 var movie = [
   {
-    cover: oldguardcover,
-    featureimage: oldguardfeature,
+    cover: "assets/images/oldguardcover.jpg",
+    featureimage: 'assets/images/OldGuardfeature.jpg',
     title: "The Old Guard",
-    titleimage: oldguardtitle,
+    titleimage: "assets/images/oldguardtitle.png",
     year: "2016",
     hours: "2h5m",
     movietype: "Action",

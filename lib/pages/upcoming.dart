35ee12ac738f4bcac524fe7dba7965d2f44cff9f
FlutLab.io <>
2021-06-25 13:51:44 +0000

    Autosave

--- lib/pages/upcoming.dart
+++ lib/pages/upcoming.dart
@@ -160,7 +160,7 @@ class upcomingState extends State<Upcoming> {
                                     )),
                                 Text("Movie Title Movie Title Movie Title Movie Title Movie TitleMovie Title Movie Title Movie Title Movie Title Movie Title Movie Title Movie Title  Movie Title Movie Title  Movie Title Movie Title  Movie Title Movie Title  Movie Title Movie Title",
                                     style: TextStyle(
-                                      fontSize: 8,
+                                      fontSize: 12,
                                       color: Colors.white,
                                     )),
                               ]),

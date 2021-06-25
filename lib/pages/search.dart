0ad296f319ce676afc8a6cf1cd9c2ea4055665cd
FlutLab.io <>
2021-06-25 11:25:07 +0000

    Autosave

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -23,173 +23,27 @@ class searchState extends State<Search> {
   }
 
   Widget getBody() {
-    //var size = MediaQuery.of(context).size;
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: SingleChildScrollView(
-          child: Stack(
-        children: [
-          Container(
-            child: SafeArea(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Image.asset(
-                          "assets/images/logo.png",
-                          width: 35,
-                          fit: BoxFit.cover,
-                        ),
-                        Row(
-                          children: [
-                            IconButton(
-                              icon: Icon(
-                                Icons.arrow_back,
-                                color: Colors.white,
-                                size: 25,
-                              ),
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => RootApp()),
-                                );
-                                print("home clicked");
-                              },
-                            ), //Icon button
-                            IconButton(
-                                icon: Icon(
-                                  Icons.collections_bookmark,
-                                  color: Colors.white,
-                                  size: 25,
-                                ),
-                                onPressed: null), //Icon button
-                          ],
-                        ), // inner row
-                      ],
-                    ), //row
-                  ), //padding
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 80,
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                            top: 18,
-                            left: 4,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Upcoming Movies",
-                                  style: TextStyle(
-                                    fontSize: 26,
-                                    color: Colors.white,
-                                  )),
-                              Container(
-                                height: 80,
-                                child: TextField(
-                                  controller: _controller,
-                                  onSubmitted: (String value) async {
-                                    await showDialog<void>(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return AlertDialog(
-                                          title: const Text('Thanks!'),
-                                          content: Text('You typed "$value", which has length ${value.characters.length}.'),
-                                          actions: <Widget>[
-                                            TextButton(
-                                              onPressed: () {
-                                                Navigator.pop(context);
-                                              },
-                                              child: const Text('OK'),
-                                            ),
-                                          ],
-                                        );
-                                      },
-                                    );
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ), //Row
-
-                  SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 18,
-                      ),
-                      child: Column(
-                          children: List.generate(12, (index) {
-                        return InkWell(
-                          onTap: () {
-                            print("container clicked $index");
-                          },
-                          child: new Container(
-                            decoration: BoxDecoration(color: Colors.black38),
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                top: 4,
-                                bottom: 4,
-                              ),
-                              child: Row(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                                Column(children: [
-                                  Image.asset(
-                                    "assets/images/search_1.jpg",
-                                    width: 150,
-                                    height: 80,
-                                  )
-                                ]),
-                                Column(children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                      left: 15,
-                                      top: 8,
-                                    ),
-                                    child: Column(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                                      Text("Indexed Movie Title",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 15,
-                                          )),
-                                      Text("About movie Detial",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 12,
-                                          )),
-                                      Text("2016 | +11 | 2016 | Action",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 12,
-                                          )),
-                                    ]),
-                                  )
-                                ]),
-                              ]),
-                            ),
-                            //height: 100,
-                          ),
-                        ); //InkWell
-                      }) //list
-                          ), //row
-                    ),
-                  ), //scroll view
-                ], //children
-              ), //col
-            ),
-          ), //container
-        ],
-      )), //Stack //SingleChildScrolview
-    ); //padding
+    return TextField(
+      controller: _controller,
+      onSubmitted: (String value) async {
+        await showDialog<void>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Thanks!'),
+              content: Text('You typed "$value", which has length ${value.characters.length}.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
   }
 }

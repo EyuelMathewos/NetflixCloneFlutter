d193a323d32f0ec63871fe7545567ed2dc0a7dee
FlutLab.io <>
2021-06-26 06:40:00 +0000

    Autosave

--- lib/JSON/movie.dart
+++ lib/JSON/movie.dart
@@ -1,13 +1,13 @@
 var movie = [
   {
-    cover: 'assets/images/oldguardcover.jpg',
-    featureimage: 'assets/images/OldGuardfeature.jpg',
-    title: "The Old Guard",
-    titleimage: 'assets/images/oldguardtitle.png',
-    year: "2016",
-    hours: "2h5m",
-    movietype: "Action",
-    about: "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
-    url: "5ed36e35c80cb808ee742a2b"
+    'cover': 'assets/images/oldguardcover.jpg',
+    'featureimage': 'assets/images/OldGuardfeature.jpg',
+    'title': "The Old Guard",
+    'titleimage': 'assets/images/oldguardtitle.png',
+    'year': "2016",
+    'hours': "2h5m",
+    'movietype': "Action",
+    'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
+    'url': "5ed36e35c80cb808ee742a2b"
   }
 ];

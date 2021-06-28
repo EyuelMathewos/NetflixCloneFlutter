f3a4c2a1bd64e89a20e9afc63b2e8332ca88bb54
FlutLab.io <>
2021-06-28 11:48:02 +0000

    Autosave

--- lib/JSON/movie.dart
+++ lib/JSON/movie.dart
@@ -44,3 +44,95 @@ List movie = [
     'url': "5ed36e35c80cb808ee742a2b"
   }
 ];
+List movie2 = [
+  {
+    'cover': 'assets/images/oldguardcover.jpg',
+    'featureimage': 'assets/images/OldGuardfeature.jpg',
+    'title': "The Old Guard",
+    'titleimage': 'assets/images/oldguardtitle.png',
+    'year': "2016",
+    'hours': "2h5m",
+    'movietype': "Action",
+    'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
+    'url': "5ed36e35c80cb808ee742a2b"
+  },
+  {
+    'cover': 'assets/images/assassinscreedcover.jpg',
+    'featureimage': 'assets/images/assassinscreedfeature.jpg',
+    'title': "Assassins Creed",
+    'titleimage': 'assets/images/assassinscreedtitle.png',
+    'year': "2016",
+    'hours': "2h5m",
+    'movietype': "Action",
+    'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
+    'url': "5ed36e35c80cb808ee742a2b"
+  },
+  {
+    'cover': 'assets/images/spider-mancover.jpg',
+    'featureimage': 'assets/images/spider-manfeature.jpg',
+    'title': "Spider-Man: Far from Home",
+    'titleimage': 'assets/images/spidermantitle.png',
+    'year': "2016",
+    'hours': "2h5m",
+    'movietype': "Action",
+    'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
+    'url': "5ed36e35c80cb808ee742a2b"
+  },
+  {
+    'cover': 'assets/images/venomcover.jpg',
+    'featureimage': 'assets/images/venomfeature.jpg',
+    'title': "Assassins Creed",
+    'titleimage': "Venom",
+    'year': "2016",
+    'hours': "2h5m",
+    'movietype': "Action",
+    'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
+    'url': "5ed36e35c80cb808ee742a2b"
+  }
+];
+List movie3 = [
+  {
+    'cover': 'assets/images/oldguardcover.jpg',
+    'featureimage': 'assets/images/OldGuardfeature.jpg',
+    'title': "The Old Guard",
+    'titleimage': 'assets/images/oldguardtitle.png',
+    'year': "2016",
+    'hours': "2h5m",
+    'movietype': "Action",
+    'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
+    'url': "5ed36e35c80cb808ee742a2b"
+  },
+  {
+    'cover': 'assets/images/assassinscreedcover.jpg',
+    'featureimage': 'assets/images/assassinscreedfeature.jpg',
+    'title': "Assassins Creed",
+    'titleimage': 'assets/images/assassinscreedtitle.png',
+    'year': "2016",
+    'hours': "2h5m",
+    'movietype': "Action",
+    'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
+    'url': "5ed36e35c80cb808ee742a2b"
+  },
+  {
+    'cover': 'assets/images/spider-mancover.jpg',
+    'featureimage': 'assets/images/spider-manfeature.jpg',
+    'title': "Spider-Man: Far from Home",
+    'titleimage': 'assets/images/spidermantitle.png',
+    'year': "2016",
+    'hours': "2h5m",
+    'movietype': "Action",
+    'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
+    'url': "5ed36e35c80cb808ee742a2b"
+  },
+  {
+    'cover': 'assets/images/venomcover.jpg',
+    'featureimage': 'assets/images/venomfeature.jpg',
+    'title': "Assassins Creed",
+    'titleimage': "Venom",
+    'year': "2016",
+    'hours': "2h5m",
+    'movietype': "Action",
+    'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
+    'url': "5ed36e35c80cb808ee742a2b"
+  }
+];

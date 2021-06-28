97740aad4f2668e612d07660679bd1a421f816b1
FlutLab.io <>
2021-06-28 13:14:48 +0000

    Autosave

--- lib/pages/upcoming.dart
+++ lib/pages/upcoming.dart
@@ -163,7 +163,7 @@ class upcomingState extends State<Upcoming> {
                                     right: 8,
                                   ),
                                   child: Column(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                                    Text(movie[index][title],
+                                    Text(movie[index]["title"],
                                         style: TextStyle(
                                           fontSize: 24,
                                           color: Colors.white,

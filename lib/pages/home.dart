316c0ba1958058896ad6bfd42e38cefbe2ecb624
FlutLab.io <>
2021-06-28 13:08:22 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,7 +24,7 @@ class homePageState extends State<Home> {
             child: Text(name,
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 15,
+                  fontSize: 20,
                   //fontWeight: FontWeight.bold,
                 )),
           ),

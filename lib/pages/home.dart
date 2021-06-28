20ebb56b2fe75457afbd73e00e33613a70e822a8
FlutLab.io <>
2021-06-28 12:59:06 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,7 +20,7 @@ class homePageState extends State<Home> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.all(15),
+            padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
             child: Text(name,
                 style: TextStyle(
                   color: Colors.white,

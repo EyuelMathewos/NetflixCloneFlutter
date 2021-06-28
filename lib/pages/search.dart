2fcc9a4bad850429ff8c414a94a124da24570c56
FlutLab.io <>
2021-06-28 14:03:35 +0000

    Autosave

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -199,7 +199,7 @@ class searchState extends State<Search> {
                                           right: 8,
                                         ),
                                         child: Container(
-                                          width: 200,
+                                          width: 150,
                                           child: Text(movie[index]["about"],
                                               style: TextStyle(
                                                 color: Colors.white,

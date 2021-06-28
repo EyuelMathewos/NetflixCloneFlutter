c37d4373570bc8973bc0c20a2c1750f667766528
FlutLab.io <>
2021-06-28 13:06:26 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,7 +24,7 @@ class homePageState extends State<Home> {
             child: Text(name,
                 style: TextStyle(
                   color: Colors.white,
-                  fontWeight: FontWeight.bold,
+                  //fontWeight: FontWeight.bold,
                 )),
           ),
           SingleChildScrollView(

6c5b9f87e2b0c1ad74655a4ddb115645daa9640b
FlutLab.io <>
2021-06-29 08:37:32 +0000

    Autosave

--- lib/JSON/movie.dart
+++ lib/JSON/movie.dart
@@ -8,7 +8,7 @@ List movie = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60788a9c7577ba0015ae4c39"
   },
   {
     'cover': 'assets/images/assassinscreedcover.jpg',
@@ -19,7 +19,7 @@ List movie = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "In this adaptation of the critically acclaimed video game franchise, a man on death row is rescued from execution by a mysterious organization.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60788b53990bb300157cced8"
   },
   {
     'cover': 'assets/images/spider-mancover.jpg',
@@ -30,7 +30,7 @@ List movie = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "Even your friendly neighborhood superhero can use a vacation. But a new threat forces Peter Parker to swing into action during a school trip to Europe.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60788fb914a3570015e0237b"
   },
   {
     'cover': 'assets/images/venomcover.jpg',
@@ -41,7 +41,7 @@ List movie = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "A reporter battles a mad scientist in a fight for his life after merging with a snarky alien symbiote that gives him remarkable superpowers.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "6078908e7d1c2c001558151e"
   }
 ];
 List movie2 = [
@@ -54,7 +54,7 @@ List movie2 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "An ex-soldier, a teen and a cop collide in New Orleans as they hunt for the source behind a dangerous new pill that grants users temporary superpowers",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60602ff0532bd90015d23125"
   },
   {
     'cover': 'assets/images/bloodshotcover.jpg',
@@ -65,7 +65,7 @@ List movie2 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "Ray Garrison, an elite soldier who was killed in battle, is brought back to life by an advanced technology that gives him the ability of super human strength and fast healing. With his new abilities, he goes after the man who killed his wife, or at least, who he believes killed his wife.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60602ff0532bd90015d23125"
   },
   {
     'cover': 'assets/images/lucycover.jpg',
@@ -76,7 +76,7 @@ List movie2 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "A woman, accidentally caught in a dark deal, turns the tables on her captors and transforms into a merciless warrior evolved beyond human logic.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "607892fdee1b350015dfaebe"
   },
   {
     'cover': 'assets/images/spider-mancover.jpg',
@@ -87,7 +87,7 @@ List movie2 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "Even your friendly neighborhood superhero can use a vacation. But a new threat forces Peter Parker to swing into action during a school trip to Europe.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60788fb914a3570015e0237b"
   },
   {
     'cover': 'assets/images/venomcover.jpg',
@@ -98,7 +98,7 @@ List movie2 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "A reporter battles a mad scientist in a fight for his life after merging with a snarky alien symbiote that gives him remarkable superpowers.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "6078908e7d1c2c001558151e"
   }
 ];
 List movie3 = [
@@ -111,7 +111,7 @@ List movie3 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "In this adaptation of the critically acclaimed video game franchise, a man on death row is rescued from execution by a mysterious organization.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60788b53990bb300157cced8"
   },
   {
     'cover': 'assets/images/oldguardcover.jpg',
@@ -122,7 +122,7 @@ List movie3 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "Four undying warriors who've secretly protected humanity for centuries become targeted for their mysterious powers just as they discover a new immortal.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60788a9c7577ba0015ae4c39"
   },
   {
     'cover': 'assets/images/spider-mancover.jpg',
@@ -133,7 +133,7 @@ List movie3 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "Even your friendly neighborhood superhero can use a vacation. But a new threat forces Peter Parker to swing into action during a school trip to Europe.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60788fb914a3570015e0237b"
   },
   {
     'cover': 'assets/images/bloodshotcover.jpg',
@@ -144,7 +144,7 @@ List movie3 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "Ray Garrison, an elite soldier who was killed in battle, is brought back to life by an advanced technology that gives him the ability of super human strength and fast healing. With his new abilities, he goes after the man who killed his wife, or at least, who he believes killed his wife.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60602ff0532bd90015d23125"
   },
   {
     'cover': 'assets/images/lucycover.jpg',
@@ -155,7 +155,7 @@ List movie3 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "A woman, accidentally caught in a dark deal, turns the tables on her captors and transforms into a merciless warrior evolved beyond human logic.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "607892fdee1b350015dfaebe"
   },
   {
     'cover': 'assets/images/projectpowercover.jpg',
@@ -166,7 +166,7 @@ List movie3 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "An ex-soldier, a teen and a cop collide in New Orleans as they hunt for the source behind a dangerous new pill that grants users temporary superpowers",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "60602ff0532bd90015d23125"
   },
   {
     'cover': 'assets/images/venomcover.jpg',
@@ -177,6 +177,6 @@ List movie3 = [
     'hours': "2h5m",
     'movietype': "Action",
     'about': "A reporter battles a mad scientist in a fight for his life after merging with a snarky alien symbiote that gives him remarkable superpowers.",
-    'url': "5ed36e35c80cb808ee742a2b"
+    'url': "6078908e7d1c2c001558151e"
   }
 ];

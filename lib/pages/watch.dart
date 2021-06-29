db85b8043ca67b86354c94fa5816e55416d84dc0
FlutLab.io <>
2021-06-29 12:40:17 +0000

    Autosave

--- lib/pages/watch.dart
+++ lib/pages/watch.dart
@@ -152,7 +152,7 @@ class watchState extends State<Watch> {
                                         fontSize: 15,
                                         fontWeight: FontWeight.bold,
                                       )),
-                                                                        Text(${widget.urllink},
+                                  Text('${widget.urllink}',
                                       style: TextStyle(
                                         color: Colors.white,
                                         fontSize: 15,

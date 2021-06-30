f7e9b05f05f7f2edfbb796b72f14679c54391c6a
FlutLab.io <>
2021-06-30 12:05:32 +0000

    Autosave

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -36,6 +36,22 @@ class loginState extends State<Login> {
               color: Colors.black,
               borderRadius: BorderRadius.circular(6),
             ),
+            child: Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter your email',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          )
           ),
         ),
       ]),

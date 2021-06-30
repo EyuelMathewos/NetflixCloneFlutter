3eefeb291844f15c557f2818efea44ca1f22e485
FlutLab.io <>
2021-06-30 12:07:26 +0000

    Autosave

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -44,12 +44,6 @@ class loginState extends State<Login> {
                     decoration: const InputDecoration(
                       hintText: 'Enter your email',
                     ),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 16.0),

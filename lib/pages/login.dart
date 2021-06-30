1c6d2ce020f6cd88a9808b2cfc1c4b5daf154901
FlutLab.io <>
2021-06-30 13:36:25 +0000

    Autosave

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -86,7 +86,7 @@ class loginState extends State<Login> {
                                   borderSide: BorderSide(color: Colors.orange),
                                 ))),
                         ConstrainedBox(
-                          constraints: BoxConstraints.tightFor(width: size.width - 60, height: 30),
+                          constraints: BoxConstraints.tightFor(width: size.width - 60, height: 50),
                           child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
                               // background color

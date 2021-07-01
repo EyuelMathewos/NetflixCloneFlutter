c381b828fec25f9aa48f9dfaf34b8bc10d5b2cbb
FlutLab.io <>
2021-07-01 11:55:57 +0000

    Autosave

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -27,7 +27,7 @@ class searchState extends State<Search> {
     List.generate(movieSearchList.length, (index) {
       print("********List values ${movieSearchList[index]['title']}");
       if (movieSearchList[index]['title'].toLowerCase().contains(value.toLowerCase())) {
-        searchedList.value.add(movieSearchList[index][0]);
+        searchedList.add(movieSearchList[index]);
         print("true");
         print(searchedList);
       }

967cf3ac9045c4969669da77abfa7103421086b8
FlutLab.io <>
2021-07-01 13:55:31 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -83,7 +83,7 @@ class homePageState extends State<Home> {
               children: [
                 Stack(children: [
                   Container(
-                    height: size.height - 120,
+                    height: size.height - 140,
                     decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/banner.webp"), fit: BoxFit.cover)),
                   ), //container
                   Container(
